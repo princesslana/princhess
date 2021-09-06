d4364d5d1663af077ed0512ac8143f1670935e84
ianagbip1oti <ianagbip1oti@gmail.com>
2021-09-06 19:13:27 +0000

    📦 NEW: Allow configuration of hash size

--- src/arena.rs
+++ src/arena.rs
@@ -9,85 +9,84 @@ use std::mem;
 use std::ops::DerefMut;
 use std::sync::Mutex;
 
+#[derive(Debug)]
+pub enum ArenaError {
+    Full,
+}
+
+const CHUNK_SIZE: usize = 1 << 21; // 2MB
+
 pub struct Arena {
-    owned_slices: Mutex<LinkedList<Box<[u8]>>>,
     owned_mappings: Mutex<LinkedList<MmapMut>>,
+    max_chunks: usize,
 }
 
 impl Arena {
-    pub fn new() -> Self {
+    pub fn new(max_size_mb: usize) -> Self {
+        let max_chunks = max_size_mb << 20 / CHUNK_SIZE;
         Self {
-            owned_slices: Default::default(),
             owned_mappings: Default::default(),
+            max_chunks,
         }
     }
-    fn give_boxed(&self, mut memory: Box<[u8]>) -> &mut [u8] {
-        let result = (&mut *memory) as *mut _;
-        let mut owned_slices = self.owned_slices.lock().unwrap();
-        owned_slices.push_back(memory);
-        unsafe { &mut *result }
-    }
-    fn give_mmap(&self, mut map: MmapMut) -> &mut [u8] {
+    fn give_mmap(&self, mut map: MmapMut) -> Result<&mut [u8], ArenaError> {
         let result = map.deref_mut() as *mut _;
         let mut owned_mappings = self.owned_mappings.lock().unwrap();
-        owned_mappings.push_back(map);
-        unsafe { &mut *result }
-    }
-    fn alloc(&self, sz: usize) -> &mut [u8] {
-        if sz == 1 << 21 {
-            self.give_mmap(MmapMut::map_anon(sz).unwrap())
+        if owned_mappings.len() > self.max_chunks {
+            Err(ArenaError::Full)
         } else {
-            self.give_boxed(vec![0; sz].into_boxed_slice())
+            owned_mappings.push_back(map);
+            unsafe { Ok(&mut *result) }
         }
     }
-    pub fn allocator(&self) -> ArenaAllocator {
-        self.allocator_with_chunk_size(1 << 21) // 2 MB
+    fn alloc_chunk(&self) -> Result<&mut [u8], ArenaError> {
+        self.give_mmap(MmapMut::map_anon(CHUNK_SIZE).unwrap())
     }
-    pub fn allocator_with_chunk_size(&self, chunk_size: usize) -> ArenaAllocator {
+    pub fn allocator(&self) -> ArenaAllocator {
         ArenaAllocator {
             arena: self,
-            memory: UnsafeCell::new(self.alloc(chunk_size)),
-            chunk_size,
+            memory: UnsafeCell::new(self.alloc_chunk().expect("Could not create allocator")),
         }
     }
 }
 
 pub struct ArenaAllocator<'a> {
     arena: &'a Arena,
-    chunk_size: usize,
     memory: UnsafeCell<&'a mut [u8]>,
 }
 
 const ALIGN: usize = 8;
 
 impl<'a> ArenaAllocator<'a> {
-    fn get_memory(&self, sz: usize) -> &'a mut [u8] {
+    fn get_memory(&self, sz: usize) -> Result<&'a mut [u8], ArenaError> {
         let memory = unsafe { &mut *self.memory.get() };
         if sz <= memory.len() {
             let (left, right) = memory.split_at_mut(sz);
             unsafe { *self.memory.get() = right };
-            left
-        } else if sz > self.chunk_size {
-            self.arena.alloc(sz)
+            Ok(left)
+        } else if sz > CHUNK_SIZE {
+            Err(ArenaError::Full)
         } else {
-            *memory = self.arena.alloc(self.chunk_size);
+            *memory = self.arena.alloc_chunk()?;
             self.get_memory(sz)
         }
     }
-    pub fn alloc_one<T: Pod>(&self) -> &'a mut T {
+
+    pub fn alloc_one<T: Pod>(&self) -> Result<&'a mut T, ArenaError> {
         assert!(ALIGN % mem::align_of::<T>() == 0);
         let x = mem::size_of::<T>();
         let x = x + ((!x + 1) % ALIGN); // TODO fix panic when x=0
-        let x = self.get_memory(x);
+        let x = self.get_memory(x)?;
         let x = T::ref_from_slice_mut(x);
-        x.unwrap()
+        Ok(x.unwrap())
     }
-    pub fn alloc_slice<T: Pod>(&self, sz: usize) -> &'a mut [T] {
+
+    pub fn alloc_slice<T: Pod>(&self, sz: usize) -> Result<&'a mut [T], ArenaError> {
         assert!(ALIGN % mem::align_of::<T>() == 0);
         let x = mem::size_of::<T>();
         let x = x + ((!x + 1) % ALIGN); // TODO fix panic when x=0
-        let x = self.get_memory(x * sz);
+        let x = self.get_memory(x * sz)?;
         let x = u8::map_slice_mut(x);
-        x.unwrap()
+        Ok(x.unwrap())
     }
 }
--- src/options.rs
+++ src/options.rs
@@ -2,6 +2,7 @@ use atomics::{AtomicUsize, Ordering};
 use std::cmp::max;
 
 static NUM_THREADS: AtomicUsize = AtomicUsize::new(1);
+static HASH_SIZE_MB: AtomicUsize = AtomicUsize::new(16);
 
 pub fn set_num_threads(threads: usize) {
     NUM_THREADS.store(threads, Ordering::Relaxed);
@@ -9,4 +10,12 @@ pub fn set_num_threads(threads: usize) {
 
 pub fn get_num_threads() -> usize {
     max(1, NUM_THREADS.load(Ordering::Relaxed))
+}
+
+pub fn set_hash_size_mb(hs: usize) {
+    HASH_SIZE_MB.store(hs, Ordering::Relaxed);
+}
+
+pub fn get_hash_size_mb() -> usize {
+    max(1, HASH_SIZE_MB.load(Ordering::Relaxed))
 }
