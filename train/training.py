01440ff8d09a654b0462d4a5d6ac5c57e1bf42cc
ianagbip1oti <ianagbip1oti@gmail.com>
2021-10-16 04:02:16 +0000

    👌 IMP: Better training for state model

--- train/training.py
+++ train/training.py
@@ -1,43 +1,94 @@
 import glob
 import numpy
 import random
+import sklearn
 import sys
 from numpy import array2string
+from scipy.sparse import vstack
 from sklearn.datasets import load_svmlight_file
 from sklearn.linear_model import SGDClassifier
 
-n_repeats = 2
-n_iter = 50
 
-classifier = SGDClassifier(loss='hinge', verbose=True, fit_intercept=False, n_jobs=-1)
+def train(classifier, files):
+    all_files = list(glob.glob(files))
 
-files = glob.glob("model_data/*.libsvm.*")
+    random.shuffle(all_files)
 
-print(f"Reading from {len(files)} files...")
+    test_files = all_files[: int(len(files) / 10)]
+    train_files = all_files[int(len(files) / 10) :]
 
-files = list(files) * n_repeats
-random.shuffle(files)
+    print("Loading test data...")
+    x_test = None
+    y_test = None
+    for f in test_files:
+        print(f"Loading {f}...")
+        x, y = load_svmlight_file(f)
 
-print("Fitting...")
-for i, f in enumerate(files):
-    print(f"Loading {f} ({i + 1}/{len(files)})...")
-    x, y = load_svmlight_file(f)
-    print(f"{len(y)} samples loaded", file=sys.stderr)
+        x_test = vstack((x_test, x)) if x_test is not None else x
+        y_test = numpy.concatenate((y_test, y)) if y_test is not None else y
 
-    for i in range(n_iter):
-        print(f"Iteration {i + 1}/{n_iter}...")
-        classifier.partial_fit(x, y, classes=numpy.unique(y))
+    classes = numpy.unique(y_test)
+
+    print(f"{len(y_test)} samples in test data")
+
+    print(f"Training from {len(train_files)} files...")
+
+    test_scores = []
+    learning_rate = classifier.get_params()["eta0"]
+    n_iters_no_improvement = 0
+    best_coefs = None
+
+    while n_iters_no_improvement < 3:
+        print("Fitting...")
+        random.shuffle(train_files)
+        classifier.set_params(eta0=learning_rate)
+        for i, f in enumerate(train_files):
+            print(
+                f"Loading {f} ({i + 1}/{len(train_files)}, iteration {len(test_scores) + 1})..."
+            )
+            x, y = load_svmlight_file(f)
+            print(f"{len(y)} samples loaded")
+
+            y_shuffled, x_shuffled = sklearn.utils.shuffle(y, x)
+
+            classifier.partial_fit(x, y, classes=classes)
+
+        print("Testing...")
+        test_score = classifier.score(x_test, y_test)
 
 
-classifier.densify()
+        if test_scores:
+            if test_score < max(test_scores):
+                n_iters_no_improvement += 1
+            else:
+                n_iters_no_improvement = 0
+                classifier.densify()
+                best_coefs = classifier.coef_.transpose()
+        else:
+            classifier.densify()
+            best_coefs = classifier.coef_.transpose()
+
+        test_scores.append(test_score)
+        learning_rate /= 2
+
+        print(f"Test Scores: {test_scores}")
+
+    return best_coefs
+
+
+classifier = SGDClassifier(
+    loss="log",
+    verbose=True,
+    fit_intercept=False,
+    n_jobs=3,
+    eta0=0.01,
+    learning_rate="constant",
+    alpha=1e-6,
+)
+
+coefs = train(classifier, files="model_data/*.libsvm.*")
 
 numpy.set_printoptions(threshold=numpy.inf)
 
 with open("model", "w") as f:
-    print(array2string(classifier.coef_.transpose(), separator=","), file=f)
-
-
-
-
-
-
+    print(array2string(coefs, separator=","), file=f)
