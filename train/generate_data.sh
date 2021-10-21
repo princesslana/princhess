87593b17d45816205fe3f6563ff51428678c3b72
ianagbip1oti <ianagbip1oti@gmail.com>
2021-10-21 01:52:37 +0000

    📦 NEW: Add Bishop pair to evaluation

--- train/generate_data.sh
+++ train/generate_data.sh
@@ -12,10 +12,15 @@ do
 
   echo "Splitting data..."
 
+  rm -f model_data/*.libsvm.*
   split -l 1000000 train_data.libsvm model_data/$(basename $f).libsvm.
 
   rm train_data.libsvm
+  rm -f model_data/*.gz
 done
+
+
+
 
 
 
