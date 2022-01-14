6ad31e6e5a766a26fbc4a630b097c7289a1a95a7
ianagbip1oti <ianagbip1oti@gmail.com>
2022-01-14 16:24:10 +0000

    📦 NEW: state.768x64x1.e081-l0.58.h5

--- train/training.py
+++ train/training.py
@@ -73,71 +73,17 @@ def load_files(fs):
     print(f"{len(all_y)} samples loaded.")
     return (all_x.todense(), all_y)
 
-def model_builder(hp):
-    decimals = [1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7]
-    hp_hidden_layer = hp.Choice('hidden_layer', values=[32, 64, 128, 256, 512, 1024])
-    hp_learning_rate = hp.Choice("learning_rate", values=decimals)
-    #hp_decay_steps = hp.Choice("decay_steps", values=[1e4, 1e5, 1e6, 1e7, 1e8])
-    #hp_decay_rate = hp.Float('decay_rate', min_value=0.5, max_value=1.0)
-    hp_init = hp.Choice('initializer', values=['zeros', 'ones'])
-    hp_output_bias = hp.Boolean("output_bias")
-    #hp_l1 = hp.Choice("l1", values=[0.0] + decimals)
-    #hp_l2 = hp.Choice("l2", values=[0.0] + decimals)
-    #hp_beta_1 = hp.Float('beta_1', min_value=0.75, max_value=1.0)
-    #hp_beta_2 = hp.Float('beta_2', min_value=0.9, max_value=1.0)
-
-
-    model = keras.Sequential()
-    model.add(keras.Input(shape=(768,)))
-    model.add(layers.Dense(hp_hidden_layer, activation="relu", kernel_initializer=hp_init))
-                           #kernel_regularizer=keras.regularizers.l1_l2(l1=hp_l1, l2=hp_l2)))
-    model.add(layers.Dense(3, activation="softmax", kernel_initializer=hp_init, use_bias=hp_output_bias))
-    model.summary()
-
-    #lr = keras.optimizers.schedules.ExponentialDecay(hp_learning_rate,
-    #                                                 decay_steps=hp_decay_steps,
-    #                                                 decay_rate=hp_decay_rate)
-    optimizer = keras.optimizers.Adam(learning_rate=hp_learning_rate)
-    #optimizer = keras.optimizers.SGD(learning_rate=lr)
-
-    model.compile(
-        optimizer=optimizer, loss="sparse_categorical_crossentropy", metrics=["acc"]
-    )
-
-    return model
-
-def tune_with_keras(files):
+def train_state_with_keras(files):
     train_files, test_files = split_files_train_and_test(list(glob.glob(files)))
     batch_size = 256
     test_data = load_files(test_files)
     train_generator = generate_batches(files=train_files, batch_size=batch_size)
 
-    tuner = kt.Hyperband(model_builder, objective="val_loss", max_epochs=50, factor=3, project_name="state_tuning")
-
-    es = keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
-
-    tuner.search(train_generator, epochs=200, callbacks=[es], verbose=1,
-                 validation_data = test_data,
-                 steps_per_epoch=len(train_files) * 1000000 / batch_size / 2)
-
-    best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
-
-    print("Best:")
-    for param in ['hidden_layer', 'learning_rate', 'initializer', 'output_bias']:
-        print(f"{param}: {best_hps.get(param)}")
-
-
-def train_with_keras(files):
-    train_files, test_files = split_files_train_and_test(list(glob.glob(files)))
-    batch_size = 256
-    test_data = load_files(test_files)
-    train_generator = generate_batches(files=train_files, batch_size=batch_size)
-
-    hidden_layers=32
+    hidden_layers=64
 
     model = keras.Sequential()
     model.add(keras.Input(shape=(768,)))
-    model.add(layers.Dense(hidden_layer, activation="relu", kernel_initializer="he_normal"))
+    model.add(layers.Dense(hidden_layers, activation="relu", kernel_initializer="he_normal"))
     model.add(layers.Dense(1, activation="tanh", kernel_initializer="he_normal", use_bias=False))
     model.summary()
 
@@ -146,78 +92,76 @@ def train_with_keras(files):
     model.compile(optimizer=optimizer, loss="mean_squared_error")
 
     mc = ModelCheckpoint(
-        filepath="checkpoints/state.768x" + str(hidden_layers) + "e{epoch:03d}-l{val_loss:.2f}.h5",
+        filepath="checkpoints/state.768x" + str(hidden_layers) + "x1.e{epoch:03d}-l{val_loss:.2f}.h5",
         verbose=True,
+        montior="val_loss",
+        save_best_only=True,
     )
+    es = EarlyStopping(monitor='val_loss', patience=15, verbose=True)
 
     model.fit(
         train_generator,
         epochs=500,
         verbose=1,
-        callbacks=[mc],
+        callbacks=[mc, es],
         steps_per_epoch=len(train_files) * 1000000 / batch_size,
         validation_data=test_data
     )
 
 
-def train(classifier, files):
-    train_files = list(glob.glob(files))
+def train_policy_with_keras(key, files):
+    train_files, test_files = split_files_train_and_test(list(glob.glob(files)))
+    batch_size = 256
+    test_data = load_files(test_files)
+    train_generator = generate_batches(files=train_files, batch_size=batch_size)
 
-    print(f"Training from {len(train_files)} files...")
+    hidden_layers=256
 
-    for iteration in range(5):
-        print("Fitting...")
-        random.shuffle(train_files)
-        # classifier.set_params(learning_rate_init=learning_rate)
-        for i, f in enumerate(train_files):
-            print(
-                f"Loading {f} ({i + 1}/{len(train_files)}, iteration {iteration + 1})..."
-            )
-            x, y = load_svmlight_file(f)
-            print(f"{len(y)} samples loaded")
+    model = keras.Sequential()
+    model.add(keras.Input(shape=(768,)))
+    model.add(layers.Dense(hidden_layers, activation="relu", kernel_initializer="he_normal"))
+    model.add(layers.Dense(4096, activation="softmax", kernel_initializer="he_normal", use_bias=False))
+    model.summary()
 
-            y_shuffled, x_shuffled = sklearn.utils.shuffle(y, x)
+    optimizer = keras.optimizers.Adam(learning_rate=0.001)
 
-            classifier.partial_fit(x, y)
+    model.compile(optimizer=optimizer, loss="sparse_categorical_crossentropy", metrics=["acc"])
 
-    return classifier.coefs_, classifier.intercepts_
+    mc = ModelCheckpoint(
+        filepath="checkpoints/" + key + ".768x" + str(hidden_layers) + "x4096.e{epoch:03d}-l{val_loss:.2f}-a{val_acc:.2f}.h5",
+        verbose=True,
+        monitor="val_loss",
+        save_best_only=True,
+    )
+    es = EarlyStopping(monitor='val_loss', patience=15, verbose=True)
 
-
-def write_coefs(file, coefs):
-    numpy.set_printoptions(threshold=numpy.inf)
-
-    with open(file, "w") as f:
-        print(array2string(coefs, separator=","), file=f)
-
-
-def model_to_coefs(file):
-    model = keras.models.load_model(file)
-
-    hidden_weights, hidden_bias = model.layers[0].get_weights()
-    output_weights, output_bias = model.layers[1].get_weights()
-
-    write_coefs("hidden_weights", numpy.transpose(hidden_weights))
-    write_coefs("hidden_bias", hidden_bias)
-    write_coefs("output_weights", numpy.transpose(output_weights))
-    write_coefs("output_bias", output_bias)
+    model.fit(
+        train_generator,
+        epochs=500,
+        verbose=1,
+        callbacks=[mc, es],
+        steps_per_epoch=len(train_files) * 1000000 / batch_size,
+        validation_data=test_data
+    )
 
 
 def train_state():
-    #tune_with_keras(files="model_data/*.libsvm.*")
-    train_with_keras(files="model_data/*.libsvm.*")
+    train_state_with_keras(files="model_data/*.libsvm.*")
 
 
-def train_policy():
-    coefs = train(classifier, files="policy_data/*.libsvm.*")
-    coefs = numpy.ravel(coefs)
-    write_coefs("policy_model", coefs)
+def train_policy(folder):
+    train_policy_with_keras(folder, files=f"{folder}/*.libsvm.*")
 
 
 train_what = sys.argv[1] if len(sys.argv) == 2 else None
 
 if train_what == "state":
     train_state()
+elif train_what == "from":
+    train_policy("from_data")
+elif train_what == "to":
+    train_policy("to_data")
 elif train_what == "policy":
-    train_policy()
+    train_policy("policy_data")
 else:
     print("Must specify to train either 'state' or 'policy'")
