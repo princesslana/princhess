2d7c1249f9c5cb0b8c5fe4b4df0419293ca34ff4
ianagbip1oti <ianagbip1oti@gmail.com>
2022-02-13 03:13:05 +0000

    📦 NEW: state.768x32x32x1.e007-l0.39.h5

--- train/export_model.py
+++ train/export_model.py
@@ -13,11 +13,15 @@ def write_coefs(file, coefs):
 def model_to_coefs(file):
     model = keras.models.load_model(file)
 
-    hidden_weights, hidden_bias = model.layers[0].get_weights()
-    output_weights, = model.layers[1].get_weights()
+    hidden1_weights, hidden1_bias = model.layers[0].get_weights()
+    hidden2_weights, hidden2_bias = model.layers[1].get_weights()
 
-    write_coefs("hidden_weights", numpy.transpose(hidden_weights))
-    write_coefs("hidden_bias", hidden_bias)
+    output_weights, = model.layers[2].get_weights()
+
+    write_coefs("hidden1_weights", numpy.transpose(hidden1_weights))
+    write_coefs("hidden1_bias", hidden1_bias)
+    write_coefs("hidden2_weights", numpy.transpose(hidden2_weights))
+    write_coefs("hidden2_bias", hidden2_bias)
     write_coefs("output_weights", numpy.transpose(output_weights))
 
 model_to_coefs(sys.argv[1])
--- train/training.py
+++ train/training.py
@@ -72,10 +72,13 @@ def train_state_with_keras(files):
     batch_size = 256
     train_generator = generate_batches(files=train_files, batch_size=batch_size)
 
-    hidden_layers = 64
+    hidden_layers = 32
 
     model = keras.Sequential()
     model.add(keras.Input(shape=(768,)))
+    model.add(
+        layers.Dense(hidden_layers, activation="relu", kernel_initializer="he_normal")
+    )
     model.add(
         layers.Dense(hidden_layers, activation="relu", kernel_initializer="he_normal")
     )
