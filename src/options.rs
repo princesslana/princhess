e764f572f7d791a765a7831193edb7eafce15728
ianagbip1oti <ianagbip1oti@gmail.com>
2022-04-04 15:35:00 +0000

    📦 NEW: Float history update factor

--- src/options.rs
+++ src/options.rs
@@ -13,7 +13,7 @@ static CPUCT_FACTOR: Lazy<RwLock<f32>> = Lazy::new(|| RwLock::new(2.82));
 static MATE_SCORE: Lazy<RwLock<f32>> = Lazy::new(|| RwLock::new(1.1));
 
 static POLICY_UPDATE_FREQUENCY: AtomicU32 = AtomicU32::new(100);
-static POLICY_UPDATE_FACTOR: AtomicI32 = AtomicI32::new(1);
+static POLICY_UPDATE_FACTOR: Lazy<RwLock<f32>> = Lazy::new(|| RwLock::new(-0.5));
 
 pub fn set_num_threads(threads: usize) {
     NUM_THREADS.store(threads, Ordering::Relaxed);
@@ -77,10 +77,12 @@ pub fn get_policy_update_frequency() -> u32 {
     POLICY_UPDATE_FREQUENCY.load(Ordering::Relaxed)
 }
 
-pub fn set_policy_update_factor(f: i32) {
-    POLICY_UPDATE_FACTOR.store(f, Ordering::Relaxed);
+pub fn set_policy_update_factor(f: f32) {
+    let mut puf = POLICY_UPDATE_FACTOR.write().unwrap();
+    *puf = f
 }
 
-pub fn get_policy_update_factor() -> i32 {
-    POLICY_UPDATE_FACTOR.load(Ordering::Relaxed)
+pub fn get_policy_update_factor() -> f32 {
+    let puf = POLICY_UPDATE_FACTOR.read().unwrap();
+    *puf
 }
