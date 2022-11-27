57d227d119331e05b569293129ff342542e2ae69
ianagbip1oti <ianagbip1oti@gmail.com>
2022-11-27 15:22:34 +0000

    📦 NEW: Remove cpuct base and factor. Tune CPuct

--- src/options.rs
+++ src/options.rs
@@ -1,14 +1,12 @@
 use once_cell::sync::Lazy;
 use std::cmp::max;
-use std::sync::atomic::{AtomicU64, AtomicUsize, Ordering};
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::RwLock;
 
 static NUM_THREADS: AtomicUsize = AtomicUsize::new(1);
 static HASH_SIZE_MB: AtomicUsize = AtomicUsize::new(16);
 
-static CPUCT: Lazy<RwLock<f32>> = Lazy::new(|| RwLock::new(2.15));
-static CPUCT_BASE: AtomicU64 = AtomicU64::new(18368);
-static CPUCT_FACTOR: Lazy<RwLock<f32>> = Lazy::new(|| RwLock::new(2.82));
+static CPUCT: Lazy<RwLock<f32>> = Lazy::new(|| RwLock::new(1.79));
 
 pub fn set_num_threads(threads: usize) {
     NUM_THREADS.store(threads, Ordering::Relaxed);
@@ -34,22 +32,4 @@ pub fn set_cpuct(c: f32) {
 pub fn get_cpuct() -> f32 {
     let cp = CPUCT.read().unwrap();
     *cp
-}
-
-pub fn set_cpuct_base(c: u64) {
-    CPUCT_BASE.store(c, Ordering::Relaxed);
-}
-
-pub fn get_cpuct_base() -> u64 {
-    CPUCT_BASE.load(Ordering::Relaxed)
-}
-
-pub fn set_cpuct_factor(c: f32) {
-    let mut cf = CPUCT_FACTOR.write().unwrap();
-    *cf = c;
-}
-
-pub fn get_cpuct_factor() -> f32 {
-    let cf = CPUCT_FACTOR.read().unwrap();
-    *cf
 }
