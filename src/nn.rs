127823c5580eaeb4c9d607a770f464f851874ce5
ianagbip1oti <ianagbip1oti@gmail.com>
2022-12-22 05:03:07 +0000

    📦 NEW: Unify NN::get_input and get_output

--- src/nn.rs
+++ src/nn.rs
@@ -15,36 +15,34 @@ const EVAL_HIDDEN_WEIGHTS: [[f32; NUMBER_INPUTS]; NUMBER_HIDDEN] =
 const EVAL_OUTPUT_WEIGHTS: [[f32; NUMBER_HIDDEN]; NUMBER_OUTPUTS] =
     include!("model/output_weights");
 
-pub struct NN {
-    hidden_layer: [f32; NUMBER_HIDDEN],
-}
+pub struct NN;
 
 impl NN {
     pub fn new() -> Self {
-        #[allow(clippy::uninit_assumed_init)]
-        let hidden_layer = unsafe { std::mem::MaybeUninit::uninit().assume_init() };
-        Self { hidden_layer }
+        Self
     }
 
-    pub fn set_inputs(&mut self, inputs: &[f32; state::NUMBER_FEATURES]) {
-        self.hidden_layer.copy_from_slice(&EVAL_HIDDEN_BIAS);
+    pub fn get_output(&self, inputs: &[f32; state::NUMBER_FEATURES]) -> f32 {
+        #[allow(clippy::uninit_assumed_init)]
+        let mut hidden_layer: [f32; NUMBER_HIDDEN] =
+            unsafe { std::mem::MaybeUninit::uninit().assume_init() };
+
+        hidden_layer.copy_from_slice(&EVAL_HIDDEN_BIAS);
 
         for i in 0..inputs.len() {
             if inputs[i] > 0.5 {
-                for j in 0..self.hidden_layer.len() {
-                    self.hidden_layer[j] += EVAL_HIDDEN_WEIGHTS[j][i];
+                for j in 0..hidden_layer.len() {
+                    hidden_layer[j] += EVAL_HIDDEN_WEIGHTS[j][i];
                 }
             }
         }
-    }
 
-    pub fn get_output(&self) -> f32 {
         let mut result = 0.;
 
         let weights = EVAL_OUTPUT_WEIGHTS[0];
 
-        for i in 0..self.hidden_layer.len() {
-            result += weights[i] * self.hidden_layer[i].max(0.);
+        for i in 0..hidden_layer.len() {
+            result += weights[i] * hidden_layer[i].max(0.);
         }
 
         result
