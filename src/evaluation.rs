ae0b472548338f0046091005c672a7e52d6fdf28
ianagbip1oti <ianagbip1oti@gmail.com>
2022-12-30 06:46:31 +0000

    👌 IMP: Remove Evaluator trait and GooseEval

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -1,7 +1,7 @@
 use chess::*;
 use math;
-use mcts::{Evaluator, GameState};
-use search::{GooseMcts, SCALE};
+use mcts::GameState;
+use search::SCALE;
 use shakmaty::Position;
 use shakmaty_syzygy::Wdl;
 use state::{self, MoveList, Player, State};
@@ -11,50 +11,40 @@ use tablebase::probe_tablebase_wdl;
 
 const MATE_FACTOR: f32 = 1.1;
 
-pub struct GooseEval;
-
-impl GooseEval {
-    pub fn new() -> Self {
-        Self
-    }
-}
-
-impl Evaluator<GooseMcts> for GooseEval {
-    fn evaluate_new_state(&self, state: &State, moves: &MoveList) -> (Vec<f32>, i64, bool) {
-        let features = state.features();
-        let move_evaluations = evaluate_policy(state, &features, moves);
-        let mut tb_hit = false;
-        let state_evaluation = if moves.is_empty() {
-            let x = (MATE_FACTOR * SCALE) as i64;
-            if state.shakmaty_board().is_check() {
-                match state.current_player() {
-                    chess::Color::White => -x,
-                    chess::Color::Black => x,
-                }
-            } else {
-                0
-            }
-        } else if let Some(wdl) = probe_tablebase_wdl(state.shakmaty_board()) {
-            tb_hit = true;
-            let win_score = SCALE as i64;
-            match (wdl, state.board().side_to_move()) {
-                (Wdl::Win, Color::White) => win_score,
-                (Wdl::Loss, Color::White) => -win_score,
-                (Wdl::Win, Color::Black) => -win_score,
-                (Wdl::Loss, Color::Black) => win_score,
-                _ => 0,
+pub fn evaluate_new_state(state: &State, moves: &MoveList) -> (Vec<f32>, i64, bool) {
+    let features = state.features();
+    let move_evaluations = evaluate_policy(state, &features, moves);
+    let mut tb_hit = false;
+    let state_evaluation = if moves.is_empty() {
+        let x = (MATE_FACTOR * SCALE) as i64;
+        if state.shakmaty_board().is_check() {
+            match state.current_player() {
+                chess::Color::White => -x,
+                chess::Color::Black => x,
             }
         } else {
-            (evaluate_state(state, &features) * SCALE) as i64
-        };
-        (move_evaluations, state_evaluation, tb_hit)
-    }
-
-    fn interpret_evaluation_for_player(&self, evaln: &i64, player: &Player) -> i64 {
-        match *player {
-            Color::White => *evaln,
-            Color::Black => -*evaln,
+            0
         }
+    } else if let Some(wdl) = probe_tablebase_wdl(state.shakmaty_board()) {
+        tb_hit = true;
+        let win_score = SCALE as i64;
+        match (wdl, state.board().side_to_move()) {
+            (Wdl::Win, Color::White) => win_score,
+            (Wdl::Loss, Color::White) => -win_score,
+            (Wdl::Win, Color::Black) => -win_score,
+            (Wdl::Loss, Color::Black) => win_score,
+            _ => 0,
+        }
+    } else {
+        (evaluate_state(state, &features) * SCALE) as i64
+    };
+    (move_evaluations, state_evaluation, tb_hit)
+}
+
+pub fn interpret_evaluation_for_player(evaln: &i64, player: &Player) -> i64 {
+    match *player {
+        Color::White => *evaln,
+        Color::Black => -*evaln,
     }
 }
 
