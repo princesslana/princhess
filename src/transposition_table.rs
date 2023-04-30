de6c34f3f80fc0b6cfc7fb619160b4509b94dcf0
ianagbip1oti <ianagbip1oti@gmail.com>
2023-04-30 08:15:46 +0000

    📦 NEW: Only track visits and eval on moveinfo

--- src/transposition_table.rs
+++ src/transposition_table.rs
@@ -70,7 +70,6 @@ impl TranspositionTable {
 
     pub fn lookup_into(&self, state: &State, dest: &mut SearchNode) {
         if let Some(src) = self.lookup(state) {
-            dest.replace(src);
             dest.set_evaln(*src.evaln());
 
             let lhs = dest.hots();
