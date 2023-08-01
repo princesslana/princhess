259aa5410c0ad279b48ec45d43c1e645262f9b61
ianagbip1oti <ianagbip1oti@gmail.com>
2023-08-01 12:43:17 +0000

    📦 NEW: Avoid evaluating state twice

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -58,15 +58,15 @@ impl From<Evaluation> for i64 {
     }
 }
 
-pub fn evaluate_state_only(state: &State) -> Evaluation {
+pub fn evaluate_state(state: &State) -> Evaluation {
     let state_evaluation = Evaluation::from(run_eval_net(state));
     state
         .side_to_move()
         .fold_wb(state_evaluation, state_evaluation.flip())
 }
 
-pub fn evaluate_new_state(state: &State, moves: &MoveList) -> (Vec<f32>, Evaluation) {
-    let (state_evaluation, move_evaluations) = run_nets(state, moves);
+pub fn evaluate_policy(state: &State, moves: &MoveList) -> (Vec<f32>, Evaluation) {
+    let move_evaluations = run_policy_net(state, moves);
 
     let state_evaluation = if moves.is_empty() {
         if state.board().is_check() {
@@ -81,7 +81,7 @@ pub fn evaluate_new_state(state: &State, moves: &MoveList) -> (Vec<f32>, Evaluat
             _ => TB_DRAW,
         }
     } else {
-        Evaluation::from(state_evaluation)
+        Evaluation::Scaled(0)
     };
 
     (
@@ -141,12 +141,11 @@ fn run_eval_net(state: &State) -> f32 {
     result.tanh()
 }
 
-fn run_nets(state: &State, moves: &MoveList) -> (f32, Vec<f32>) {
+fn run_policy_net(state: &State, moves: &MoveList) -> Vec<f32> {
     let mut evalns = Vec::with_capacity(moves.len());
 
     if moves.is_empty() {
-        // Returning 0 is ok here, as we'll immediately check for why there's no moves
-        return (0., evalns);
+        return evalns;
     }
 
     let mut move_idxs = Vec::with_capacity(moves.len());
@@ -156,23 +155,7 @@ fn run_nets(state: &State, moves: &MoveList) -> (f32, Vec<f32>) {
         evalns.push(0.);
     }
 
-    let mut hidden_layer: [f32; NUMBER_HIDDEN] = unsafe {
-        let mut out: [MaybeUninit<f32>; NUMBER_HIDDEN] = MaybeUninit::uninit().assume_init();
-
-        ptr::copy_nonoverlapping(
-            EVAL_HIDDEN_BIAS.as_ptr(),
-            out.as_mut_ptr().cast::<f32>(),
-            NUMBER_HIDDEN,
-        );
-
-        mem::transmute(out)
-    };
-
     state.features_map(|idx| {
-        for (j, l) in hidden_layer.iter_mut().enumerate() {
-            *l += EVAL_HIDDEN_WEIGHTS[idx][j];
-        }
-
         for m in 0..moves.len() {
             evalns[m] += POLICY_WEIGHTS[move_idxs[m]][idx];
         }
@@ -180,12 +163,5 @@ fn run_nets(state: &State, moves: &MoveList) -> (f32, Vec<f32>) {
 
     math::softmax(&mut evalns);
 
-    let mut result = 0.;
-    let weights = EVAL_OUTPUT_WEIGHTS[0];
-
-    for i in 0..hidden_layer.len() {
-        result += weights[i] * hidden_layer[i].max(0.);
-    }
-
-    (result.tanh(), evalns)
+    evalns
 }
