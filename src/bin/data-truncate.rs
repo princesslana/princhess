f672b7647e9fca5a4816a7e85f7e219dee023468
Princess Lana <ianagbip1oti@gmail.com>
2024-03-11 07:13:23 +0200

    📦 NEW: New value net. Add data truncation script

---
+++ src/bin/data-truncate.rs
@@ -0,0 +1,64 @@
+use princhess::train::TrainingPosition;
+
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, BufWriter, Write};
+use std::time::Instant;
+
+const BUFFER_COUNT: usize = 1 << 16;
+
+fn main() {
+    let mut args = env::args();
+    args.next();
+
+    let truncate_to = args.next().unwrap().parse::<usize>().unwrap();
+    let files = args.collect::<Vec<String>>();
+
+    for input in files {
+        let file = File::open(input.clone()).unwrap();
+        let positions = file.metadata().unwrap().len() as usize / TrainingPosition::SIZE;
+
+        let buffer_size = BUFFER_COUNT * TrainingPosition::SIZE;
+        let mut buffer = BufReader::with_capacity(buffer_size, file);
+
+        let out_file = format!("{}.{}m.truncated", input, truncate_to);
+        let mut writer =
+            BufWriter::new(File::create(out_file).unwrap());
+
+        let start = Instant::now();
+        println!(
+            "Truncating {} positions from {} to {}m positions...",
+            positions, input, truncate_to
+        );
+
+        let mut processed = 0;
+
+        while let Ok(bytes) = buffer.fill_buf() {
+            if bytes.is_empty() {
+                break;
+            }
+
+            let data = TrainingPosition::read_batch(bytes);
+            TrainingPosition::write_batch(&mut writer, data).unwrap();
+
+            processed += data.len();
+
+            print!(
+                "{:>8} / {} ({:2}%)\r",
+                processed,
+                truncate_to * 1000000,
+                processed * 100 / (truncate_to * 1000000),
+            );
+            io::stdout().flush().unwrap();
+
+            let consumed = bytes.len();
+            buffer.consume(consumed);
+
+            if processed >= truncate_to * 1000000 {
+                break;
+            }
+        }
+
+        println!("Wrote {}m positions in {:?}", truncate_to, start.elapsed());
+    }
+}
