e95d923172a2c7d7c72f5f31bd17c3d214f13d6c
ianagbip1oti <ianagbip1oti@gmail.com>
2024-03-11 05:07:33 +0000

    📦 NEW: New value net. Add data truncation script

--- src/bin/data-truncate.rs
+++ src/bin/data-truncate.rs
@@ -1,9 +1,8 @@
-use princhess::state::State;
 use princhess::train::TrainingPosition;
 
-use std::io::{self, BufRead, BufReader, BufWriter, Write};
 use std::env;
 use std::fs::File;
+use std::io::{self, BufRead, BufReader, BufWriter, Write};
 use std::time::Instant;
 
 const BUFFER_COUNT: usize = 1 << 16;
@@ -12,22 +11,27 @@ fn main() {
     let mut args = env::args();
     args.next();
 
+    let truncate_to = args.next().unwrap().parse::<usize>().unwrap();
     let files = args.collect::<Vec<String>>();
 
     for input in files {
         let file = File::open(input.clone()).unwrap();
-        let count = file.metadata().unwrap().len() as usize / TrainingPosition::SIZE;
+        let positions = file.metadata().unwrap().len() as usize / TrainingPosition::SIZE;
 
         let buffer_size = BUFFER_COUNT * TrainingPosition::SIZE;
         let mut buffer = BufReader::with_capacity(buffer_size, file);
 
-        let mut writer = BufWriter::new(File::create(format!("{}.filtered", input)).unwrap());
+        let out_file = format!("{}.{}m.truncated", input, truncate_to);
+        let mut writer =
+            BufWriter::new(File::create(out_file).unwrap());
 
         let start = Instant::now();
-        println!("Filtering {} positions from {}...", count, input);
+        println!(
+            "Truncating {} positions from {} to {}m positions...",
+            positions, input, truncate_to
+        );
 
         let mut processed = 0;
-        let mut filtered = 0;
 
         while let Ok(bytes) = buffer.fill_buf() {
             if bytes.is_empty() {
@@ -35,34 +39,26 @@ fn main() {
             }
 
             let data = TrainingPosition::read_batch(bytes);
-
-            for position in data {
-                let state = State::from(position);
-
-                if state.is_check() {
-                    filtered += 1;
-                    continue;
-                }
-
-                TrainingPosition::write_batch(&mut writer, &[*position]).unwrap();
-            }
+            TrainingPosition::write_batch(&mut writer, data).unwrap();
 
             processed += data.len();
 
             print!(
-                "{:>8} / {} ({:2}%) / {} ({:2}%)\r",
+                "{:>8} / {} ({:2}%)\r",
                 processed,
-                count,
-                processed * 100 / count,
-                filtered,
-                filtered * 100 / processed
+                truncate_to * 1000000,
+                processed * 100 / (truncate_to * 1000000),
             );
             io::stdout().flush().unwrap();
 
             let consumed = bytes.len();
             buffer.consume(consumed);
+
+            if processed >= truncate_to * 1000000 {
+                break;
+            }
         }
 
-        println!("Filtered {} positions in {:?}", count, start.elapsed());
+        println!("Wrote {}m positions in {:?}", truncate_to, start.elapsed());
     }
 }
