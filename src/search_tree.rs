887d827c3890b45b5a7dd07be18fddb6acf287c2
Princess Lana <ianagbip1oti@gmail.com>
2025-04-21 12:58:24 -0700

    Revert "📦 NEW: Remove locks on main playout path"
    This reverts commit 3f199a195c009ff82078b96fea12f104e251f092.

--- src/search_tree.rs
+++ src/search_tree.rs
@@ -311,6 +311,8 @@ impl SearchTree {
         let generation = self.ttable.generation();
 
         loop {
+            self.ttable.wait_if_flipping();
+
             if self.ttable.generation() != generation {
                 return true;
             }
