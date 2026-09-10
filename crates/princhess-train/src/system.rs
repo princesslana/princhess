fa32c73df0fd80372afec1d8e76afc70c60ac385
ianagbip1oti <ianagbip1oti@gmail.com>
2026-09-10 14:14:44 -0700

    👌 IMP: Cap physical cores by available CPU count

--- crates/princhess-train/src/system.rs
+++ crates/princhess-train/src/system.rs
@@ -1,5 +1,5 @@
 #[must_use]
 pub fn default_thread_count() -> u16 {
-    let physical = num_cpus::get_physical() as u16;
+    let physical = num_cpus::get_physical().min(num_cpus::get()) as u16;
     physical.saturating_sub(1).max(1)
 }
